7c529f701d75a34671bf4a0268aa522f1d50c1de
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-04 15:22:32 +0200

    Changed background-color

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -14,8 +14,6 @@ class App extends React.Component {
     fetch("http://api.sr.se/api/v2/channels?format=json&size=100").then((response) => {
       return response.json()
     }).then((json) => {
-      console.log(json)
-      console.log(json.channels)
       this.setState({ radioItems: json.channels })
     })
   }
@@ -25,7 +23,8 @@ class App extends React.Component {
       return (
         <div>
           <div className="header-info">
-            <img src="./sr-logo.jpg" alt="sr-logo" className="sr-logo" />
+            <img src="./sr-image.jpg" alt="sr-logo" className="sr-image" />
+            <input type="text" placeholder="Sök.."className="search-input" />
           </div>
           <div className="radioPlayer">
             {this.state.radioItems.map((channel) => {
